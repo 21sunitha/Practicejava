ab5608e6d83e0f0b0d2bef406fac5baff6171ccd
sunit <sunit@SUNITHADEVEN>
2025-06-26 19:59:06 +0530

    Simple program for comparison of two strings.

---
+++ PracticeJava/src/CampareString/SuryaMeg.java
@@ -0,0 +1,18 @@
+package CampareString;
+import java.util.Scanner;
+public class SuryaMeg {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		String meghana=sc.nextLine();
+		if(meghana.equals("death")) {
+			System.out.println("Surya meets Ramya");
+		}
+		else {
+			System.out.println("Surya weds meghana");
+		}
+
+	}
+
+}
