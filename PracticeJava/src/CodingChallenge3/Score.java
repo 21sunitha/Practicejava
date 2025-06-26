27cfc51219f5b6398da7f7bada64efa6378d8f06
sunit <sunit@SUNITHADEVEN>
2025-06-26 21:47:47 +0530

    Score using else if .

---
+++ PracticeJava/src/CodingChallenge3/Score.java
@@ -0,0 +1,20 @@
+package CodingChallenge3;
+
+public class Score {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int score=75;
+		if(score<50) {
+			System.out.println("you need to improve");
+		}
+		else if(score>50&&score<70) {
+			System.out.println("good job");
+		}
+		else if(score>70) {
+			System.out.println("excellent performance");
+		}
+
+	}
+
+}
