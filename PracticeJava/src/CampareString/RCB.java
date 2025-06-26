b5347ffd2cac1e80c5e844e3b17c3029f776e710
sunit <sunit@SUNITHADEVEN>
2025-06-26 19:53:27 +0530

    Comparing of two strings.

---
+++ PracticeJava/src/CampareString/RCB.java
@@ -0,0 +1,18 @@
+package CampareString;
+import java.util.Scanner;
+public class RCB {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		String RCB=sc.nextLine();
+		if(RCB.equals("win")) {
+			System.out.println("ee sala cup namadhey");
+		}
+		else {
+			System.out.println("cup nehi");
+		}
+
+	}
+
+}
