e1e267246880457888fb4adbc9160cea62c0cb52
sunit <sunit@SUNITHADEVEN>
2025-06-26 11:17:22 +0530

    Getting input from the user for string values.

---
+++ PracticeJava/src/samplejava/UserString.java
@@ -0,0 +1,15 @@
+package samplejava;
+import java.util.Scanner;
+
+public class UserString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		String a=sc.nextLine();
+		System.out.println(a);
+		
+
+	}
+
+}
