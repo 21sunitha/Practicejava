6b99ea7fb0ad7b947f4a7a953f903a8f394ec7e0
sunit <sunit@SUNITHADEVEN>
2025-06-26 19:43:53 +0530

    Comparing of two strings.

---
+++ PracticeJava/src/CampareString/TwoStrings.java
@@ -0,0 +1,12 @@
+package CampareString;
+
+public class TwoStrings {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String a="apple";// reference abc
+		String b="apple";//reference abc
+		String c=b;
+		System.out.println(a==c);
+	}
+}	
