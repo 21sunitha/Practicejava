24ec930838cc22a20b7f165d60f1e09851566f1f
sunit <sunit@SUNITHADEVEN>
2025-06-26 21:26:34 +0530

    Checking odd or even using if else.

---
+++ PracticeJava/src/CodingChallenge2/EvenOdd.java
@@ -0,0 +1,18 @@
+package CodingChallenge2;
+import java.util.Scanner;
+public class EvenOdd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int number=sc.nextInt();
+		if(number%2==0) {
+			System.out.println("number is even");
+		}
+		else {
+			System.out.println("number is odd");
+		}
+
+	}
+
+}
