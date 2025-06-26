23eafec7d6391337b51edcc4c06e46dbef873ea9
sunit <sunit@SUNITHADEVEN>
2025-06-26 09:47:28 +0530

    Example of variables.

---
+++ PracticeJava/src/samplejava/sampleVar.java
@@ -0,0 +1,17 @@
+package samplejava;
+
+public class sampleVar {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=79;
+		String john="j";
+		String a1="john";
+		
+		System.out.println(a);
+		System.out.println(john);
+		System.out.println(a1);
+
+	}
+
+}
