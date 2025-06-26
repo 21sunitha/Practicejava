c90339a895bca73833e95a46704d0e9eb81c04d8
sunit <sunit@SUNITHADEVEN>
2025-06-26 18:42:07 +0530

    Sample if else program in java.

---
+++ PracticeJava/src/IfElse.java
@@ -0,0 +1,17 @@
+
+public class IfElse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num1=40;
+		int num2=50;
+		if(num1>=num2) {
+			System.out.println("num1 is greater");
+		}
+		else {
+			System.out.println("num2 is greater");
+		}
+
+	}
+
+}
