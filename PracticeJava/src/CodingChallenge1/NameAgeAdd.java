c61ef9217be1181fae02340c8910f94c8b0a75f2
sunit <sunit@SUNITHADEVEN>
2025-06-26 17:40:52 +0530

    To access integer after string data type in java.

---
+++ PracticeJava/src/CodingChallenge1/NameAgeAdd.java
@@ -0,0 +1,18 @@
+package CodingChallenge1;
+import java.util.Scanner;
+public class NameAgeAdd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		String Name=sc.nextLine();
+		int Age=sc.nextInt();
+		sc.nextLine();
+		String Address=sc.nextLine();
+		System.out.println("My name is : "+Name);
+		System.out.println("My age is : "+Age);
+		System.out.println("My address is : "+Address);
+
+	}
+
+}
