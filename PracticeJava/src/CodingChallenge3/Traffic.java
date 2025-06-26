20cb23fa91d5999220950529e7c736f0cabaa19d
sunit <sunit@SUNITHADEVEN>
2025-06-26 22:08:13 +0530

    Traffic program in java using else if.

---
+++ PracticeJava/src/CodingChallenge3/Traffic.java
@@ -0,0 +1,21 @@
+package CodingChallenge3;
+
+public class Traffic {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String color="yellow";
+		if(color.equals("red")) {
+			System.out.println("stop");
+			
+		}
+		else if(color.equals("yellow")) {
+			System.out.println("wait");
+		}
+		else if(color.equals("green")) {
+			System.out.println("go");
+		}
+
+	}
+
+}
