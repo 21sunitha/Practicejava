95a2f453f7df4b5568449e305f646fc3dcdb26fd
sunit <sunit@SUNITHADEVEN>
2025-06-26 11:27:40 +0530

    question 1 in coding challenge by getting input from user.

---
+++ PracticeJava/src/CodingChallenge1/NameAge.java
@@ -0,0 +1,17 @@
+package CodingChallenge1;
+import java.util.Scanner;
+
+public class NameAge {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		String Name=sc.nextLine();
+		int Age=sc.nextInt();
+		System.out.println(Name);
+		System.out.println(Age);
+		
+
+	}
+
+}
