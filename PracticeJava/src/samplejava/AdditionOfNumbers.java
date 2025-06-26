c050c3be6853f5f1f3c24bdba65389d088bfdf72
sunit <sunit@SUNITHADEVEN>
2025-06-26 10:58:27 +0530

    Addition of numbers it includes variables,data types,operators

---
+++ PracticeJava/src/samplejava/AdditionOfNumbers.java
@@ -0,0 +1,13 @@
+package samplejava;
+
+public class AdditionOfNumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=40;
+		int b=30;
+		System.out.println(a+b);
+
+	}
+
+}
