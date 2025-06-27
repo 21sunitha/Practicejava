3e6158a53699e6520d1447d1e451c330d754fcdb
sunit <sunit@SUNITHADEVEN>
2025-06-27 18:14:24 +0530

    Printing of numbers from 1 to 10 using for loop.

---
+++ PracticeJava/src/ForLoop/printNumber.java
@@ -0,0 +1,13 @@
+package ForLoop;
+
+public class printNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=1;i<11;i=i+1) {
+			System.out.println(i);
+		}
+
+	}
+
+}
