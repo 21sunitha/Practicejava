44d2975c905fe6952341861d3ab9cffd59efe6cf
sunit <sunit@SUNITHADEVEN>
2025-06-27 20:28:00 +0530

    printing the numbers that are divisible by both 3 and 5.

---
+++ PracticeJava/src/CodingChallenge4/Q5.java
@@ -0,0 +1,15 @@
+package CodingChallenge4;
+
+public class Q5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=1;i<=100;i=i+1) {
+			if(i%3==0&&i%5==0) {
+				System.out.println(i);
+			}
+		}
+
+	}
+
+}
