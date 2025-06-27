27230520a512ea955146af8a27877bb71c47f4c7
sunit <sunit@SUNITHADEVEN>
2025-06-27 21:07:00 +0530

    Creating an array.

---
+++ PracticeJava/src/Array/CreateArray.java
@@ -0,0 +1,19 @@
+package Array;
+
+public class CreateArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String playlist[]=new String [3];
+		playlist[0]="song1";
+		System.out.println(playlist[0]);
+		playlist[1]="song2";
+		System.out.println(playlist[1]);
+		playlist[2]="song3";
+		System.out.println(playlist[2]);
+		
+		
+
+	}
+
+}
