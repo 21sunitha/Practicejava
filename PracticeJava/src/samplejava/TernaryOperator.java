72b5ad8fcd3ad8a020488afb341fde16d85550fd
sunit <sunit@SUNITHADEVEN>
2025-06-27 17:44:17 +0530

    Ternary operator in java.

---
+++ PracticeJava/src/samplejava/TernaryOperator.java
@@ -0,0 +1,12 @@
+package samplejava;
+
+public class TernaryOperator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String result=5>3?"yes":"no";
+		System.out.println(result);
+
+	}
+
+}
