b8c421212e18f08bed2511d928b65a340b71a120
sunit <sunit@SUNITHADEVEN>
2025-06-27 21:20:52 +0530

    Integer array is created.

---
+++ PracticeJava/src/Array/IntegerArray.java
@@ -0,0 +1,19 @@
+package Array;
+import java.util.Scanner;
+public class IntegerArray {
+
+	public static void main(String[] args) {
+		int[]arr=new int[5];
+		Scanner sc=new Scanner(System.in);
+		arr[0]=sc.nextInt();
+		arr[1]=sc.nextInt();
+		arr[2]=sc.nextInt();
+		arr[3]=sc.nextInt();
+		arr[4]=sc.nextInt();
+	
+		System.out.println(arr[0]+arr[1]+arr[2]+arr[3]+arr[4]);
+		
+
+	}
+
+}
