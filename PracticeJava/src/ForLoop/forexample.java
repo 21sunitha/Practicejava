5d3c864bf44b5fc1079bd5b1bcc43047e2b12b65
sunit <sunit@SUNITHADEVEN>
2025-06-27 18:09:00 +0530

    For loop in java.

---
+++ PracticeJava/src/ForLoop/forexample.java
@@ -0,0 +1,14 @@
+package ForLoop;
+
+public class forexample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for(int i=1;i<=10;i=i+1) {
+			System.out.println("john");
+		}
+
+	}
+
+}
