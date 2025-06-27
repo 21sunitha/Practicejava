c9005a064adccf822ba0b8a544fbf40fe0a0575e
sunit <sunit@SUNITHADEVEN>
2025-06-27 18:22:39 +0530

    This program contain a question about print number from 5 to 10 by accessing the values of a and b from the user.

---
+++ PracticeJava/src/CodingChallenge4/Q1.java
@@ -0,0 +1,18 @@
+package CodingChallenge4;
+import java.util.Scanner;
+public class Q1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter number1 : ");
+		int a=sc.nextInt();
+		System.out.println("enter number2 : ");
+		int b=sc.nextInt();
+		for( int i=a;i<=b;i=i+1) {
+			System.out.println(i);
+		}
+
+	}
+
+}
