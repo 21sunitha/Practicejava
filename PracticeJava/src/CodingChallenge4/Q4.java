5cb3a32b4e1cc24fa5c15e4db5bc3910be5edab7
sunit <sunit@SUNITHADEVEN>
2025-06-27 20:23:56 +0530

    Counting of even numbers from 1 to 10.

---
+++ PracticeJava/src/CodingChallenge4/Q4.java
@@ -0,0 +1,16 @@
+package CodingChallenge4;
+
+public class Q4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int evencount=0;
+		for(int i=1;i<=10;i=i+1) {
+			if(i%2==0) {
+				evencount=evencount+1;
+			}
+		}
+     System.out.println(evencount);
+	}
+
+}
