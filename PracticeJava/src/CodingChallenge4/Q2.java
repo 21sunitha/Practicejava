b936b3591c970aec1bae58c9d471a06917c9139a
sunit <sunit@SUNITHADEVEN>
2025-06-27 18:30:02 +0530

    Printing of even numbers from 1 to 10 by using for loop and if else.

---
+++ PracticeJava/src/CodingChallenge4/Q2.java
@@ -0,0 +1,13 @@
+package CodingChallenge4;
+
+public class Q2 {
+	public static void main(String[]args) {
+		for(int i=0;i<=10;i=i+1) {
+			if(i%2==0) {
+				System.out.println(i);
+			}
+		
+		}
+	}
+
+}
