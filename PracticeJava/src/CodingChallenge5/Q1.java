75a4f2e9ab5ede302d420c1df36c3b6990272805
sunit <sunit@SUNITHADEVEN>
2025-06-27 21:39:15 +0530

    creating an integer array of 5 elements using for loop.

---
+++ PracticeJava/src/CodingChallenge5/Q1.java
@@ -0,0 +1,21 @@
+package CodingChallenge5;
+import java.util.Scanner;
+public class Q1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int[]arr=new int[5];
+		for(int i=0; i<=4;i++) {
+			arr[i]=sc.nextInt();
+			
+		}
+		System.out.println(arr[0]);
+		System.out.println(arr[1]);
+		System.out.println(arr[2]);
+		System.out.println(arr[3]);
+		System.out.println(arr[4]);
+
+	}
+
+}
