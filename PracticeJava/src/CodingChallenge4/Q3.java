0461e944f517385b903d591e211752376afb4862
sunit <sunit@SUNITHADEVEN>
2025-06-27 20:21:03 +0530

    Counting of odd numbers from 1 to 10.

---
+++ PracticeJava/src/CodingChallenge4/Q3.java
@@ -0,0 +1,23 @@
+package CodingChallenge4;
+
+public class Q3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int oddcount=0;
+		for(int i=1;i<=10;i=i+1) {
+			if(i%2==0) {
+				
+			}
+			else {
+				
+			
+			oddcount=oddcount+1;
+			}
+			
+		}
+		System.out.println(oddcount);
+
+	}
+
+}
