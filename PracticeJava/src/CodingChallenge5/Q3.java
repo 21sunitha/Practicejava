3af827a8b303511c2e0b36038fea47fb2ce04940
sunit <sunit@SUNITHADEVEN>
2025-06-28 09:31:22 +0530

    Printing the 2 table from 1 to 10.

---
+++ PracticeJava/src/CodingChallenge5/Q3.java
@@ -0,0 +1,11 @@
+package CodingChallenge5;
+
+public class Q3 {
+	public static void main(String[]args) {
+		int[]table=new int[10];
+		for(int i=1;i<=10;i=i+1) {
+			System.out.println(i +" x 2 ="+i*2);
+		}
+	}
+
+}
