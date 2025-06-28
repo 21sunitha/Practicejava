14da13191075aa981171cd13cb26dcba7399472a
sunit <sunit@SUNITHADEVEN>
2025-06-28 09:24:29 +0530

    Printing 1 to 10 numbers from an array.

---
+++ PracticeJava/src/CodingChallenge5/Q2.java
@@ -0,0 +1,14 @@
+package CodingChallenge5;
+
+public class Q2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[]arr=new int [10];
+		for(int i=1;i<=10;i=i+1) {
+			System.out.println(i);
+		}
+
+	}
+
+}
