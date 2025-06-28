1aa8dcdee7eb114510bd9f6d1ff7639814ac0820
sunit <sunit@SUNITHADEVEN>
2025-06-28 10:55:43 +0530

    Pattern program using nested for loop

---
+++ PracticeJava/src/ForLoop/NestedForPattern.java
@@ -0,0 +1,17 @@
+package ForLoop;
+
+public class NestedForPattern {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int j=1;j<=3;j=j+1) {
+			for(int i=1;i<=3;i=i+1) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		   
+
+	}
+
+}
