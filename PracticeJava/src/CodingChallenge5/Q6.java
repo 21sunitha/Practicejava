0f0ec4d042415ee91af7902d8fd35631b5bf2715
sunit <sunit@SUNITHADEVEN>
2025-06-28 09:39:01 +0530

    This is a special program in which getting a single input from the user and printing the whole table.

---
+++ PracticeJava/src/CodingChallenge5/Q6.java
@@ -0,0 +1,17 @@
+package CodingChallenge5;
+import java.util.Scanner;
+public class Q6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int []table=new int[10];
+		int t=sc.nextInt();
+		for(int i=1;i<=10;i+=1) {
+			System.out.println(i+"x7="+i*t);
+			
+		}
+
+	}
+
+}
