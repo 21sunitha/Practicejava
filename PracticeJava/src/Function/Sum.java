d7cc97508d8926e78e178e15e5f1e74d86f14d94
sunit <sunit@SUNITHADEVEN>
2025-06-29 21:53:32 +0530

    Simple addition function using parameterized function.

---
+++ PracticeJava/src/Function/Sum.java
@@ -0,0 +1,16 @@
+package Function;
+
+public class Sum {
+	void sum(int a,int b)
+	{
+		System.out.println(a+b);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Sum obj1=new Sum();
+		obj1.sum(20, 30);
+
+	}
+
+}
