a66198498193b7906342629b7ab1f48076769019
sunit <sunit@SUNITHADEVEN>
2025-06-29 18:57:33 +0530

    Classes and Object in java

---
+++ PracticeJava/src/ClassAndObject/Laptop.java
@@ -0,0 +1,31 @@
+package ClassAndObject;
+
+public class Laptop {
+		String name="";
+		String proc="";
+		int ram=0;
+		int price=0;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Laptop lap1=new Laptop();
+		lap1. name="Hp";
+		lap1. proc="i5";
+		lap1. ram=7;
+		lap1. price=45000;
+		
+		Laptop lap2=new Laptop();
+		lap2. name="Hp";
+		lap2. proc="i5";
+		lap2. ram=7;
+		lap2. price=45000;
+
+		
+		
+		
+		System.out.println(lap1.name);
+
+
+	}
+
+}
