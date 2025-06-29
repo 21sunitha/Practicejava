4cff49517ec821e3819be6b31273df13b2452392
sunit <sunit@SUNITHADEVEN>
2025-06-29 21:18:27 +0530

    This example shows that if function method contains static keyword means without using object we can directly access the main method.

---
+++ PracticeJava/src/Function/StaticExample.java
@@ -0,0 +1,17 @@
+package Function;
+
+public class StaticExample {
+	static void apple()
+	{
+		System.out.println("This is an apple Iphone");
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		apple();
+		
+	}
+
+	
+}
