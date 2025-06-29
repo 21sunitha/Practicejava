0013a78fc58cc67e978fa6dfe5a19f2778b66dc9
sunit <sunit@SUNITHADEVEN>
2025-06-29 21:14:59 +0530

    Functions examples.

---
+++ PracticeJava/src/Function/Example.java
@@ -0,0 +1,26 @@
+package Function;
+
+public class Example {
+	void apple()
+	{
+		System.out.println("This is an apple Iphone");
+	}
+	void greet()
+	{
+		System.out.println("hello");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Example a=new Example();
+		a.apple();
+		Example b=new Example();
+		b.greet();
+	}
+}
+
+
+
+
+
+
---
+++ PracticeJava/src/Function/FunctionDemo.java
@@ -0,0 +1,16 @@
+package Function;
+
+public class FunctionDemo {
+	void apple()
+	{
+		System.out.println("This is an apple Iphone");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		FunctionDemo a=new FunctionDemo();
+		a.apple();
+
+	}
+
+}
